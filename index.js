b2b05cccd94e903b8d6fed2c2f9bf4cc8dacf3a6
Simon Vanherweghe <simon.vanherweghe@gmail.com>
2023-03-02 15:28:00 +0100

    routes met rss

--- index.js
+++ index.js
@@ -1,41 +1,83 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
+import { Router } from 'worktop'
+import { listen } from 'worktop/cache'
+import { Feed } from 'feed'
+
+const API = new Router()
+
+API.add('GET', '/', async (req, res) => {
+  const data = await getData()
+
+  res.setHeader('content-type', 'application/json;charset=UTF-8')
+  res.end(JSON.stringify(data))
 })
 
-async function handleRequest(request) {
-	const resp = await fetch("http://budakitchen.be/")
+API.add('POST', '/slack', async (req, res) => {
+  const data = await getData()
+  res.setHeader('content-type', 'application/json;charset=UTF-8')
+  res.setHeader('Cache-Control', 'public,max-age=60')
 
-	const handler = new ElementHandler();
-	const lunch = await new HTMLRewriter()
-	.on("#short-menu-sec .short-menu p", handler)
-	.transform(resp)
-	.text()
+  res.end(
+    JSON.stringify({
+      response_type: `in_channel`,
+      text: `Today's lunch at Buda Kitchen 🍽 `,
+      attachments: [
+        {
+          text: data.join('\n'),
+        },
+      ],
+    }),
+  )
+})
 
-	if (handler.result.length===0) {
-		handler.result = ["Het draait in de soep", "We zitten in de puree", "De mayonaise pakt niet"];
+API.add('GET', '/rss', async (req, res) => {
+  const data = await getData()
+
+  const feed = new Feed({
+    title: 'Todays lunch at Buda Kitchen',
+    description: 'Hungry yet?',
+    id: 'https://budakitchen.be/',
+    link: 'https://budakitchen.be/',
+  })
+
+  data.forEach((item) => {
+    feed.addItem({
+      title: item,
+    })
+  })
+
+  res.end(feed.rss2())
+})
+
+const getData = async () => {
+  const resp = await fetch('http://budakitchen.be/')
+
+  const handler = new ElementHandler()
+  const lunch = await new HTMLRewriter()
+    .on('#short-menu-sec .short-menu p', handler)
+    .transform(resp)
+    .text()
+
+  if (handler.result.length === 0) {
+    handler.result = [
+      'Het draait in de soep',
+      'We zitten in de puree',
+      'De mayonaise pakt niet',
+    ]
   }
 
-	return new Response(JSON.stringify(	{
-    "response_type": `in_channel`,
-    "text": `Today's lunch at Buda Kitchen 🍽 `,
-    "attachments": [
-      {
-        "text": handler.result.join('\n')
-      }
-    ]
-  }), {
-		headers: { 	'Content-Type': 'application/json' },
-	})
+  return handler.result
 }
 
 class ElementHandler {
-	constructor(){
-		this.result = [];
-	}
+  constructor() {
+    this.result = []
+  }
 
   text(text) {
-		if(text.text.trim()!=""){
-			this.result.push(text.text)
-		}
+    if (text.text.trim() != '') {
+      this.result.push(text.text)
+    }
   }
 }
+
+listen(API.run)
