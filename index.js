756f7fa349c32945cbdda15f8ebb72bfd7fa4da8
Simon Vanherweghe <simon.vanherweghe@gmail.com>
2023-03-07 11:30:04 +0100

    KV caching and cronjob

--- index.js
+++ index.js
@@ -1,36 +1,29 @@
-import { Router } from 'worktop'
-import { listen } from 'worktop/cache'
 import { Feed } from 'feed'
+import { Router } from 'itty-router'
+import { json } from 'itty-router-extras'
 
-const API = new Router()
+const router = Router()
 
-API.add('GET', '/', async (req, res) => {
-  const data = await getData()
-
-  res.setHeader('content-type', 'application/json;charset=UTF-8')
-  res.end(JSON.stringify(data))
+router.get('/', async (_, env) => {
+  const data = await getData(env)
+  return json(data)
 })
 
-API.add('POST', '/slack', async (req, res) => {
-  const data = await getData()
-  res.setHeader('content-type', 'application/json;charset=UTF-8')
-  res.setHeader('Cache-Control', 'public,max-age=60')
-
-  res.end(
-    JSON.stringify({
-      response_type: `in_channel`,
-      text: `Today's lunch at Buda Kitchen 🍽 `,
-      attachments: [
-        {
-          text: data.join('\n'),
-        },
-      ],
-    }),
-  )
+router.post('/slack', async (_, env) => {
+  const data = await getData(env)
+  return json({
+    response_type: `in_channel`,
+    text: `Today's lunch at Buda Kitchen 🍽 `,
+    attachments: [
+      {
+        text: data.join('\n'),
+      },
+    ],
+  })
 })
 
-API.add('GET', '/rss', async (req, res) => {
-  const data = await getData()
+router.get('/rss', async (_, env) => {
+  const data = await getData(env)
 
   const feed = new Feed({
     title: 'Todays lunch at Buda Kitchen',
@@ -45,27 +38,61 @@ API.add('GET', '/rss', async (req, res) => {
     })
   })
 
-  res.end(feed.rss2())
+  return new Response(feed.rss2())
 })
 
-const getData = async () => {
-  const resp = await fetch('http://budakitchen.be/')
+router.all('*', () => new Response('Not Found.', { status: 404 }))
 
-  const handler = new ElementHandler()
-  const lunch = await new HTMLRewriter()
-    .on('#short-menu-sec .short-menu p', handler)
-    .transform(resp)
-    .text()
+const leadingZero = (n) => `${n}`.padStart(2, '0')
 
-  if (handler.result.length === 0) {
-    handler.result = [
-      'Het draait in de soep',
-      'We zitten in de puree',
-      'De mayonaise pakt niet',
-    ]
+const getDateKey = () => {
+  const today = new Date()
+  return (
+    '' +
+    today.getFullYear() +
+    leadingZero(today.getMonth() + 1) +
+    leadingZero(today.getDate())
+  )
+}
+
+const getData = async (env) => {
+  const dateKey = getDateKey()
+  let result = [
+    'Het draait in de soep',
+    'We zitten in de puree',
+    'De mayonaise pakt niet',
+  ]
+
+  let storedMenu = null
+
+  try {
+    storedMenu = await env.budakitchen.get(dateKey, { type: 'json' })
+
+    if (!storedMenu) {
+      console.log('storedMenu is null')
+      const resp = await fetch('http://budakitchen.be/')
+
+      const handler = new ElementHandler()
+      const lunch = await new HTMLRewriter()
+        .on('#short-menu-sec .short-menu p', handler)
+        .transform(resp)
+        .text()
+
+      if (handler.result.length > 0) {
+        await env.budakitchen.put(dateKey, JSON.stringify(handler.result), {
+          expirationTtl: 20 * 60 * 60,
+        })
+      }
+
+      result = handler.result
+    } else {
+      result = storedMenu
+    }
+  } catch (e) {
+    console.log(e)
   }
 
-  return handler.result
+  return result
 }
 
 class ElementHandler {
@@ -80,4 +107,9 @@ class ElementHandler {
   }
 }
 
-listen(API.run)
+export default {
+  scheduled: async (event, env, ctx) => {
+    ctx.waitUntil(getData(env))
+  },
+  fetch: router.handle,
+}
