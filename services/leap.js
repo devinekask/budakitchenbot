660099cef5d00b9c9f6d236fec1e603c7e3fa8df
Simon Vanherweghe <simon.vanherweghe@gmail.com>
2023-03-22 08:55:22 +0100

    width & height must be multiple of 8

--- services/leap.js
+++ services/leap.js
@@ -15,7 +15,7 @@ const generateImg = async (key, prompt) => {
         negativePrompt: 'asymmetric, watermarks',
         steps: 50,
         width: 800,
-        height: 300,
+        height: 304,
         numberOfImages: 1,
         promptStrength: 7,
         // seed: 4523184,
